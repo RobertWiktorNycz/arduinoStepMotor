1d469e7fbfd961973aba87df2bbfdf18b1b8582b
Robert Nycz <robert.wiktor.nycz@gmail.com>
2019-09-17 19:42:21 +0100

    Full code

--- arduinoStepMotor/arduinoStepMotor.ino
+++ arduinoStepMotor/arduinoStepMotor.ino
@@ -1,9 +1,18 @@
+#include<Stepper.h>
+#define STEPS 2038
+Stepper stp(STEPS, 8,10,9,11);
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+stp.setSpeed(1);
+stp.step(2038);
+delay(1000);
+stp.setSpeed(6);
+stp.step(-2038);
+delay(1000);
 
 }
